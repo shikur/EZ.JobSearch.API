4e512f8d79b9ddb3c246abd22feffbc69d34a10b
shikur <shikur.yennus@gmail.com>
2022-02-18 19:48:53 -0800

    update cv commit

---
+++ JobSearch/JobSearch/src/main/java/com/Ez/JobSearch/JobSearch/model/Address.java
@@ -0,0 +1,35 @@
+package com.Ez.JobSearch.JobSearch.model;
+
+public class Address {
+	
+	private String Address;
+	private String Address1;
+	private String City;
+	private String Country;
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getAddress1() {
+		return Address1;
+	}
+	public void setAddress1(String address1) {
+		Address1 = address1;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getCountry() {
+		return Country;
+	}
+	public void setCountry(String country) {
+		Country = country;
+	}
+	
+
+}
---
+++ JobSearch/JobSearch/src/main/java/com/Ez/JobSearch/JobSearch/model/Client.java
@@ -0,0 +1,63 @@
+package com.Ez.JobSearch.JobSearch.model;
+
+public class Client {
+	private String firstName;
+	private String lastName;
+	private String addiionalName;
+	private String pronouns;
+	private String headLine ;
+	private String currentPositions;
+	private String indutry;
+	private String locations;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddiionalName() {
+		return addiionalName;
+	}
+	public void setAddiionalName(String addiionalName) {
+		this.addiionalName = addiionalName;
+	}
+	public String getPronouns() {
+		return pronouns;
+	}
+	public void setPronouns(String pronouns) {
+		this.pronouns = pronouns;
+	}
+	public String getHeadLine() {
+		return headLine;
+	}
+	public void setHeadLine(String headLine) {
+		this.headLine = headLine;
+	}
+	public String getCurrentPositions() {
+		return currentPositions;
+	}
+	public void setCurrentPositions(String currentPositions) {
+		this.currentPositions = currentPositions;
+	}
+	public String getIndutry() {
+		return indutry;
+	}
+	public void setIndutry(String indutry) {
+		this.indutry = indutry;
+	}
+	public String getLocations() {
+		return locations;
+	}
+	public void setLocations(String locations) {
+		this.locations = locations;
+	}
+	
+	
+}
